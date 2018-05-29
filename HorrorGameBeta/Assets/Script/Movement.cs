14085042dd98de9eb7a08608a029275a9a49c06d
Imacutekayx <imkayx.yt@gmail.com>
2018-05-29 15:53:03 +0200

    Finalisation de l'environnement de base & Création du personnage

---
+++ HorrorGameBeta/Assets/Script/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 2;
+    public float turn = 2;
+    private float turning;
+	
+	// Update is called once per frame
+	public void Update () {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * speed;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * speed;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * speed;
+        }
+        turning = turn * Input.GetAxis("Mouse X");
+        transform.Rotate(0, turning, 0);
+    }
+}
