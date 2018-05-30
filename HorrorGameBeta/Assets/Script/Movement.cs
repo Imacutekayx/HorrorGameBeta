a0c91fe03cd20dc63ced4e738ce83a746365b9ac
Imacutekayx <imkayx.yt@gmail.com>
2018-05-30 09:29:36 +0200

    Finalisation des mouvement basiques du joueur

--- HorrorGameBeta/Assets/Script/Movement.cs
+++ HorrorGameBeta/Assets/Script/Movement.cs
@@ -4,42 +4,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    public float speed = 0.2f;
-    public float turn = 2f;
-    private float turning;
-    private bool down;
-	
-	// Update is called once per frame
-	public void Update () {
-        //if (Input.GetKeyDown("left ctrl"))
-        //{
-        //    transform.position -= transform.up * speed;
-        //}
-        //else if(Input.GetKeyDown("left shift"))
-        //{
-        //    speed = speed * 1.5f;
-        //}
-        //else
-        //{
-        //    speed = speed / 1.5f;
-        //}
-        if (Input.GetKey("w"))
-        {
-            transform.position += transform.forward * speed;
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.position -= transform.right * speed;
-        }
-        if (Input.GetKey("s"))
-        {
-            transform.position -= transform.forward * speed;
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.position += transform.right * speed;
-        }
-        turning = turn * Input.GetAxis("Mouse X");
-        transform.Rotate(0, turning, 0);
+    public float speed = 10f;
+    private float translation;
+    private float straffe;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    public void Update ()
+    {
+        translation = Input.GetAxis("Vertical") * speed;
+        straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
     }
 }
