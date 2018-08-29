cfb365862a6fd7f5f065438aacbed094c96bf480
Imkayx <imkayxy@gmail.com>
2018-08-29 23:16:23 +0200

    Fix of the bug of Depth always rushing at the Player && test of implementation of other start locations

--- HorrorGameBeta/Assets/Script/AI/Depth/RushPlayer.cs
+++ HorrorGameBeta/Assets/Script/AI/Depth/RushPlayer.cs
@@ -12,6 +12,12 @@ public class RushPlayer : MonoBehaviour {
     public int speed;
     private float timer;
     private bool isRushing = false;
+    private bool isOut = false;
+    private bool isSafe = false;
+    private float x;
+    private float y;
+    private float z;
+    private byte startLoc;
 
 	//Use this for initialization
 	void Start () {
@@ -21,22 +27,132 @@ public class RushPlayer : MonoBehaviour {
 	
 	//Update is called once per frame
 	void Update () {
-        if (!isRushing)
+        //TODO Fix bug rotation && after limit
+        //Check the state of Depth
+        if (!isOut)
         {
-            if (++timer * Time.deltaTime > 5)
+            if (!isRushing)
             {
-                timer = 0;
-                isRushing = true;
-                transform.SetPositionAndRotation(player.transform.position + new Vector3(0, -2000, 0), Quaternion.identity);
+                //If the timer is over, start rushing the player
+                if (++timer * Time.deltaTime > 5)
+                {
+                    timer = 0;
+                    startLoc = (byte)Random.Range(0, 3);
+                    isRushing = true;
+
+                    //Teleport the Depth depending of the startLoc
+                    switch (startLoc)
+                    {
+                        //Down
+                        case 0:
+                            {
+                                transform.SetPositionAndRotation(player.transform.position + new Vector3(0, -2500, 0), Quaternion.Euler(0f, 0f, 0f));
+                                break;
+                            }
+                        //Up
+                        case 1:
+                            {
+                                transform.SetPositionAndRotation(player.transform.position + new Vector3(0, 2500, 0), Quaternion.Euler(180f, 0f, 0f));
+                                break;
+                            }
+                        //Left
+                        case 2:
+                            {
+                                transform.SetPositionAndRotation(player.transform.position + new Vector3(0, 0, 2500), Quaternion.Euler(-90f, 0f, 0f));
+                                break;
+                            }
+                        //Right
+                        case 3:
+                            {
+                                transform.SetPositionAndRotation(player.transform.position + new Vector3(0, 0, -2500), Quaternion.Euler(90f, 0f, 0f));
+                                break;
+                            }
+                    }
+                    transform.LookAt(player.transform.position);
+                }
+            }
+            else
+            {
+                //If the timer is over, analyse the next target location
+                if (++timer * Time.deltaTime > 0.05)
+                {
+                    timer = 0;
+
+                    //Check if Depth is out of world
+                    switch (startLoc)
+                    {
+                        //Check y
+                        case 0:
+                        case 1:
+                            {
+                                if (CheckDifference(player.transform.position.y, transform.position.y) < 500 &&
+                                    CheckDifference(player.transform.position.y, transform.position.y) > -500)
+                                {
+                                    isRushing = false;
+                                    isOut = true;
+                                    if (startLoc == 0)
+                                    {
+                                        transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, 3000, 0), speed);
+                                    }
+                                    else
+                                    {
+                                        transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, -3000, 0), speed);
+                                    }
+                                }
+                                break;
+                            }
+                        //Check z
+                        case 2:
+                        case 3:
+                            {
+                                if (CheckDifference(player.transform.position.z, transform.position.z) < 500 &&
+                                    CheckDifference(player.transform.position.z, transform.position.z) > -500)
+                                {
+                                    isRushing = false;
+                                    isOut = true;
+                                    if (startLoc == 2)
+                                    {
+                                        transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, 0, -3000), speed);
+                                    }
+                                    else
+                                    {
+                                        transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, 0, 3000), speed);
+                                    }
+                                }
+                                break;
+                            }
+                    }
+
+                    if (!isOut)
+                    {
+                        //Analyse the distance with each safePoint
+                        foreach (GameObject point in safePoints)
+                        {
+                            x = CheckDifference(player.transform.position.x, point.transform.position.x);
+                            z = CheckDifference(player.transform.position.z, point.transform.position.z);
+                            if (Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(z, 2)) + 2.5 < safeDistance)
+                            {
+                                isSafe = true;
+                            }
+                        }
+
+                        //Check if the player is in a safePoint
+                        if (!isSafe)
+                        {
+                            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+                        }
+                        isSafe = false;
+                    }
+                }
             }
         }
         else
         {
-            if(transform.position.y > 5000)
+            if(++timer * Time.deltaTime > 10)
             {
-                isRushing = false;
+                timer = 0;
+                isOut = false;
             }
-            //TODO Analyse position of player compared to safePoints
         }
     }
 
@@ -45,7 +161,26 @@ public class RushPlayer : MonoBehaviour {
     {
         if(collision.collider.tag == "Player")
         {
-            //TODO GameOver
+            isRushing = false;
+            transform.SetPositionAndRotation(player.transform.position + new Vector3(0, -2500, 0), Quaternion.identity);
+        }
+    }
+
+    /// <summary>
+    /// Calculate the difference between two points
+    /// </summary>
+    /// <param name="player">Coordinate of the player</param>
+    /// <param name="point">Coordinate of the safePoint</param>
+    /// <returns>Difference</returns>
+    private float CheckDifference(float player, float point)
+    {
+        if(player > point)
+        {
+            return player - point;
+        }
+        else
+        {
+            return point - player;
         }
     }
 }
---
+++ HorrorGameBeta/Assets/Script/AI/Depth/SafePointsSpawn.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Script which create the safePoints
+/// </summary>
+public class SafePointsSpawn : MonoBehaviour {
+
+    //Objects
+    public GameObject safe;
+    public GameObject safeO2;
+    private GameObject temp;
+
+    //Variables
+    private int space;
+    private int nbrPoints = 10;
+    private int rngResult;
+    private float lastZ;
+
+    //TODO delete Update
+    private bool first = true;
+    private void Update()
+    {
+        if (first)
+        {
+            first = false;
+            Spawn();
+        }
+    }
+
+    /// <summary>
+    /// Method which calculate the position of the safePoints, theirs attributes and create them
+    /// </summary>
+    public void Spawn()
+    {
+        for(int x = 0; x < nbrPoints; ++x)
+        {
+            rngResult = Random.Range(1, 100);
+            if(space == 2 || (space == 1 && rngResult < 50) || space == 0 && rngResult < 25)
+            {
+                space = 0;
+                temp = Instantiate(safeO2);
+            }
+            else
+            {
+                ++space;
+                temp = Instantiate(safe);
+            }
+
+            temp.tag = "SafePoints";
+            temp.transform.SetPositionAndRotation(new Vector3(200 * x + 200, 0, lastZ += Random.Range(5, 50)), Quaternion.identity);
+        }
+
+        //TODO CreateEnd
+    }
+}
